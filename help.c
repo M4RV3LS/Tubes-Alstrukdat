ada90ca95045066a0b13d275a282fd44c99ddfa3
RakaAF123 <110679603+RakaAF123@users.noreply.github.com>
2022-10-31 23:10:52 +0700

    Add files via upload

---
+++ help.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int HELP()
+{
+    printf("Berikut command-command yang dapat anda gunakan: \n");
+    printf("1. START \n2. LOAD \n3. SAVE \n4. CREATE GAME \n5. LIST GAME \n6. DELETE GAME \n7. QUEUE GAME \n8. PLAY GAME \n9. SKIP GAME \n10. QUIT \n11. HELP");
+    
+    return 0;
+}
+
+
