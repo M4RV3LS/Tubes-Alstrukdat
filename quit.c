e04100ff95f75b4fb96dcf07c03278857c554954
carissarianda <16521473@mahasiswa.itb.ac.id>
2022-11-01 21:38:22 +0700

    quit

---
+++ quit.c
@@ -0,0 +1,11 @@
+#include "quit.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void QUIT() {
+    printf("Anda keluar dari game BNMO.\n");
+    printf("Bye bye ...\n");
+    exit(0);
+
+}
---
+++ quit.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#ifndef QUIT_H
+#define QUIT_H
+
+void QUIT();
+/*Keluar dari permainan.*/
+
+#endif
