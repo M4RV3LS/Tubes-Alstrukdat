4f2b5e3ff63d22c09f1a09377f87e8b0afe22b8d
RakaAF123 <rakaadmiharfan@gmail.com>
2022-11-02 23:50:52 +0700

    INI DARI RAKA

--- deletegame.c
+++ deletegame.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "creategame.h"
-#include "deletegame.h"
-#include "listgame.h"
+#include "start.h"
+#include "mesinkarakterv2.h"
+#include "mesinkata.h"
 
 void DELETE(List *game)
 {
     int nomor_game;
-    ListGame(&game);
-    
     printf("Masukkan nomor game yang akan dihapus: ");
-    fscanf(stdin, "%d", nomor_game);
+    STARTWORD();
+    nomor_game = (currentWord.TabWord - '0');
 
-    if (game->A[nomor_game] > game->A[5])
+    if (nomor_game > 5)
     {
+        int j = game->neff;
+        int i = nomor_game-1;
+        
+        while (i < j)
+        {
+            game->A[i] = game->A[i+1];
+            i++;
+        }
+        
         game->neff -= 1;
         printf("Game berhasil dihapus");
+
     }
     else
     {
