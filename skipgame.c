879343a9f0c325e5dae4de56b6c01e23f59daa76
Kinanti Wening Asih <78632601+KinantiWening1@users.noreply.github.com>
2022-11-07 12:36:04 +0700

    Add files via upload

---
+++ skipgame.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "skipgame.h"
+
+void SKIPGAME(Queue *q, int n)
+{
+    printf("Berikut adalah daftar game-mu.\n");
+    int i;
+    for (i = 0; i < length(*q1); i++){
+        printf("%d. %s",i+1, q1->buffer[i]);
+    }
+
+    ElType x;
+    if (n<length(*q)) // valid, jumlah game yang di skip ada di range antiran
+    {
+        int i = 0;
+        while (i<n)
+        {
+            dequeue(q,&x);
+        }
+        PLAYGAME(q);
+    } else { // 
+        while(!IsEmpty(*q))
+        {
+            dequeue(q,&x);
+        } // seluruh game di skip
+        printf("Tidak ada permainan lagi dalam daftar game-mu.\n");
+    }
+}
---
+++ skipgame.h
@@ -0,0 +1,7 @@
+#include "queue.h"
+
+void SKIPGAME(Queue *q, int n);
+// SKIPGAME merupakan command yang digunakan 
+// untuk melewatkan permainan sebanyak n.
+// q adalah antrian game
+// n adalah jumlah game yang akan di skip
