c930d3817b6a4f23da52b0e98eb0b380091baf67
RakaAF123 <rakaadmiharfan@gmail.com>
2022-11-08 16:50:51 +0700

    delete game

--- src/deletegame.c
+++ src/deletegame.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "start.h"
-#include "mesinkar2.h"
-#include "mesinkata2.h"
-#include "arrayOfString.h"
+#include "deletegame.h"
 
 void DELETE(ArrayDin *ListGames)
 {
     int nomor_game;
     printf("Masukkan nomor game yang akan dihapus: ");
     STARTCOMMAND();
-    nomor_game = (currentCMD.TabWord[0] - '0');
+    nomor_game = WordToInt(currentCMD);
 
     if (nomor_game > 5)
     {
@@ -20,23 +17,40 @@ void DELETE(ArrayDin *ListGames)
         while (i < j)
         {
             ListGames->A[i] = ListGames->A[i+1];
-            free(ListGames->A[i+1]);
             i++;
         }
+        free(ListGames->A[j]);
         
         ListGames->Neff -= 1;
-        printf("Game berhasil dihapus");
+        printf("Game berhasil dihapus\n");
 
     }
     else
     {
-        printf("Game gagal dihapus");
+        printf("Game gagal dihapus\n");
     }
 
 }
 
-int main()
+/* int main()
 {   
-    ArrayDin ListGames;
+    ArrayDin ListGames = CreateDynArray();
+    ListGames.A[0] = "amjad";
+    ListGames.A[1] = "raka";
+    ListGames.A[2] = "marvel";
+    ListGames.A[3] = "marvel1";
+    ListGames.A[4] = "marvel2";
+    ListGames.A[5] = "marvel3";
+    ListGames.A[6] = "marvel4";
+    ListGames.A[7] = "marvel5";
+    ListGames.Neff = 8;
     DELETE(&ListGames);
+
+    for (int i = 0; i < ListGames.Neff;i++)
+    {
+        printf("%i. %s\n", i+1, ListGames.A[i]);
+
+    }
+    printf("mampus lu anjeng\n");
 }
+*/
