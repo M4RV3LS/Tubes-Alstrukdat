407c21aae48643ce669eadec94046437fe1bbafa
Kinanti Wening Asih <78632601+KinantiWening1@users.noreply.github.com>
2022-11-10 13:21:23 +0000

    Update dinnerdash.h

--- src/dinnerdash.h
+++ src/dinnerdash.h
@@ -1,34 +1,45 @@
 #include <stdio.h>
-//#include "queue.h"
-//#include "mesinkata2.h"
-#include "ADT/queuedinner.h"
+#include "queuedinner.h"
 
 #ifndef DINERDASH_h
 #define DINERDASH_H
 
+void DisplayPesanan(Queue q);
+// Menampilkan antrian pesanan
 
-//int saldo;
+void DisplayMasakan(Queue q);
+// Menampilkan antrian makanan
+
+void DisplaySajian(Queue q);
+// Menampilkan antrian sajian
+
+boolean isValid(Word a);
+// Memeriksa validitas command
+
+boolean isCook(Word a);
+// Memeriksa apakah command berupa COOK
+
+boolean isServe(Word a);
+// Memeriksa apakah command berupa SERVE
+
+boolean isSkip(Word a);
+// Memeriksa apakah command berupa SKIP
+
+Food generateFood(int i);
+// Autogenerate pesanan
+
+Food copyFood(Food f);
+// Menyalin info makanan
 
 void COOK(Word ID, Queue *Pesanan, Queue *Masakan);
 // Memasak makanan yang ada di urutan paling atas di
 // antrian pesanan dan memasukkannnya ke dalam antrian masakan
 
-void SERVE(Queue *Pesanan, Queue *Sajian);
+void SERVE(Queue *Pesanan, Queue *Sajian, int *Saldo);
 // Menyajikan makanan yang ada di urutan paling atas di
 // antrian makanan siap saji
 
-void SKIP();
-// Melewatkan satu ronde tanpa melakukan apa-apa
-
-void DisplayPesanan();
-// Menampilkan antrian pesanan
-
-void DisplayMasakan();
-// Menampilan antrian masakan
-
-void DisplaySajian();
-// Menampilkan antrian sajian
-
-Word IsTop(Word ID);
+void dinnerdash();
+// procedure utama untuk menjalankan game dinner dash
 
 #endif
