6b992067b00dee08eb75ae5d536e284ab001545d
Kinanti Wening Asih <78632601+KinantiWening1@users.noreply.github.com>
2022-12-01 14:33:30 +0000

    Update towerofhanoi.h

--- src/towerofhanoi/towerofhanoi.h
+++ src/towerofhanoi/towerofhanoi.h
@@ -5,15 +5,24 @@
 #include "../ADT/ArrayOfMap/arrayofmap.h"
 #include "../console.h"
 
-void scoring(int steps, int* score);
+void createDisk(int i,int jml_pir, Disk* a);
+/* Membuat piringan berdasarkan besar piringan */
+
+void createStick(int jml_pir, Disk *a);
+/* Membuat tiang */
+
+void scoring(int steps, int* score, int jml_disk);
 /* Menampilkan Invalidai dari pemain sesuai steps yang dibutuhkan */
 
-void cekValiditas(char tow1, char tow2, Stack a, Stack b, Stack c, boolean *valid);
+void cekValiditas(char tow1, char tow2, STACKSS a, STACKSS b, STACKSS c, boolean *valid);
 /* Cek validitas input command game*/
 
-void displaystacks(Stack a, Stack b, Stack c);
+void displaystacks(STACKSS a, STACKSS b, STACKSS c, int jml_pir);
 /* Menampilkan tiap tower di masing-masing ronde tower of hanoi */
 
 void towerofhanoi(char* game , ArrayDin ListGames , ArrayOfMap *GameMap , int score);
 /* Memainkan game tower of hanoi */
+
+boolean win(STACKSS c, int jml_pir)
+/* Memeriksa kondisi menang */
 
