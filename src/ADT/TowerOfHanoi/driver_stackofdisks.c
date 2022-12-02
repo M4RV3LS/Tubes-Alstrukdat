626d4aff2695585057de3f4c989f15601f7ddde5
Kinanti Wening Asih <78632601+KinantiWening1@users.noreply.github.com>
2022-12-02 16:40:39 +0700

    driver

---
+++ src/ADT/TowerOfHanoi/driver_stackofdisks.c
@@ -0,0 +1,40 @@
+#include "stackofdisks.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    Stack s;
+    CreateEmptySS(&s);
+
+    //Test isempty
+    if (IsEmptySS(s))
+    {
+        printf("Awalnya, s kosong\n");
+        if (!IsFullSS(s))
+        {
+            printf("s juga tidak penuh\n");
+        }
+    }
+
+    Disk tempdii;
+    tempdii.size = 8;
+    tempdii.sym = "ini temp";
+
+    PushSS(&s,tempdii.sym,tempdii.size);
+    PushSS(&s,tempdii.sym,tempdii.size);
+    PushSS(&s,tempdii.sym,tempdii.size);
+    PushSS(&s,tempdii.sym,tempdii.size);
+    PushSS(&s,tempdii.sym,tempdii.size);
+
+    Disk tempdiii;
+    char* tempp1 = (char *) malloc (MaxEl*sizeof(char));
+    PopSS(&s,&tempdiii,5,tempp1);
+    
+    if (diskComp(tempdii,tempdiii))
+    {
+        printf("Diskcomp berhasil\n");
+    }
+
+    free(tempp1);
+}
